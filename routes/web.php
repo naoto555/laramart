244829f2ebb7020a4de72d177df81cdab690f8cc
naoto555 <takeuchi.yasuhiro@olive.plala.or.jp>
2022-09-10 06:00:06 +0900

    07 add header

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,13 @@ Route::get('/', function () {
 });
 
 Route::resource('products', 'ProductController');
+/*　↑resouceとかくと、index,edit,creare,show,update,store,delete7個(restFull)のルールを勝手に作ってくれる　*/
+/*　デメリット⇒URLが勝手に決まってします。blade　の名前もルールに従う必要あり*/
+
+/*
+Route::get('/analize', 'ProductsController@analize')->name('analize');
+            URL                         function            htmlのname
+*/
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
