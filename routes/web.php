4912cc0cbf4071b8f71b7b649e101c8021b22454
naoto555 <takeuchi.yasuhiro@olive.plala.or.jp>
2022-09-10 13:16:38 +0900

    09 create reviews

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', 'ProductController');
-/*　↑resouceとかくと、index,edit,creare,show,update,store,delete7個(restFull)のルールを勝手に作ってくれる　*/
-/*　デメリット⇒URLが勝手に決まってします。blade　の名前もルールに従う必要あり*/
+Route::post('products/{product}/reviews', 'ReviewController@store');
 
-/*
-Route::get('/analize', 'ProductsController@analize')->name('analize');
-            URL                         function            htmlのname
-*/
+Route::resource('products', 'ProductController');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
