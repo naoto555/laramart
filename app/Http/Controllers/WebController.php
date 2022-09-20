edaffa568776e8d3620210942cc098a10d0275a3
naoto555 <takeuchi.yasuhiro@olive.plala.or.jp>
2022-09-20 11:37:48 +0900

    27 Toppage_Newtopic

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Category;
+use App\Product;
 
 class WebController extends Controller
 {
     public function index()
     {
         $categories = Category::all()->sortBy('major_category_name');
-
         $major_category_names = Category::pluck('major_category_name')->unique();
+        $recently_products = Product::orderBy('created_at', 'desc')->take(4)->get();
 
-        return view('web.index', compact('major_category_names', 'categories'));
+        return view('web.index', compact('major_category_names', 'categories', 'recently_products'));
+        
     }
 }
