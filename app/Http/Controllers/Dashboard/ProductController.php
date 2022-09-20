582b4da8973bd50a286c1e5565918c7ce7aa1413
naoto555 <takeuchi.yasuhiro@olive.plala.or.jp>
2022-09-20 16:00:02 +0900

    28 Recommend_Flag

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -80,6 +80,13 @@ class ProductController extends Controller
         $product->description = $request->input('description');
         $product->price = $request->input('price');
         $product->category_id = $request->input('category_id');
+        
+        if ($request->input('recommend') == 'on') {
+            $product->recommend_flag = true;
+        } else {
+            $product->recommend_flag = false;
+        }
+        
         $product->save();
 
         return redirect()->route('dashboard.products.index');
@@ -121,6 +128,11 @@ class ProductController extends Controller
         $product->description = $request->input('description');
         $product->price = $request->input('price');
         $product->category_id = $request->input('category_id');
+        if ($request->input('recommend') == 'on') {
+            $product->recommend_flag = true;
+        } else {
+            $product->recommend_flag = false;
+        }
         $product->update();
 
         return redirect()->route('dashboard.products.index');
--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -13,8 +13,9 @@ class WebController extends Controller
         $categories = Category::all()->sortBy('major_category_name');
         $major_category_names = Category::pluck('major_category_name')->unique();
         $recently_products = Product::orderBy('created_at', 'desc')->take(4)->get();
+        $recommend_products = Product::where('recommend_flag', true)->take(3)->get();
 
-        return view('web.index', compact('major_category_names', 'categories', 'recently_products'));
+        return view('web.index', compact('major_category_names', 'categories', 'recently_products', 'recommend_products'));
         
     }
 }
